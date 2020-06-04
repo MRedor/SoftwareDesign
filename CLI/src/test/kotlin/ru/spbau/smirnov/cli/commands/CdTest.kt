8d3296e2e7a145948282b0cc84e55cbb1e0c099c
Maria Palchenkova <mashapalchenkova@gmail.com>
2020-06-04 16:07:55 +0300

    add tests

--- CLI/src/test/kotlin/ru/spbau/smirnov/cli/commands/CdTest.kt
+++ CLI/src/test/kotlin/ru/spbau/smirnov/cli/commands/CdTest.kt
@@ -1,5 +1,43 @@
 package ru.spbau.smirnov.cli.commands
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import ru.spbau.smirnov.cli.Environment
+import ru.spbau.smirnov.cli.fillFiles
+import java.nio.file.Paths
+
 class CdTest {
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun initFiles() {
+            fillFiles()
+        }
+    }
+
+    @Test
+    fun `Should change directory with no argument`() {
+        val env = Environment()
+        CommandTestUtils.runExecutorTest(
+             Cd(env, listOf()),
+            "",
+            "",
+            ""
+        )
+        assertEquals(System.getProperty("user.home"), env.currentDirectory)
+    }
+
+    @Test
+    fun `Should change directory`() {
+        val env = Environment()
+        CommandTestUtils.runExecutorTest(
+            Cd(env, listOf("src")),
+            "src",
+            "",
+            ""
+        )
+        assertEquals(Paths.get("src").toAbsolutePath().toString(), env.currentDirectory)
+    }
 
 }
--- CLI/src/test/kotlin/ru/spbau/smirnov/cli/commands/LsTest.kt
+++ CLI/src/test/kotlin/ru/spbau/smirnov/cli/commands/LsTest.kt
@@ -1,5 +1,40 @@
 package ru.spbau.smirnov.cli.commands
 
-class LsTest {
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import ru.spbau.smirnov.cli.Environment
+import ru.spbau.smirnov.cli.fillFiles
+import java.nio.file.Paths
 
+class LsTest {
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun initFiles() {
+            fillFiles()
+        }
+    }
+
+    @Test
+    fun `Should print current directory (no argument passed)`() {
+        var env = Environment()
+        env.currentDirectory = Paths.get("src").toAbsolutePath().toString()
+        CommandTestUtils.runExecutorTest(
+            Ls(env, listOf()),
+            "",
+            "main test ${System.lineSeparator()}",
+            ""
+        )
+    }
+
+    @Test
+    fun `Should print a directory` () {
+        CommandTestUtils.runExecutorTest(
+            Ls(Environment(), listOf("src")),
+            "src",
+            "main test ${System.lineSeparator()}",
+            ""
+        )
+    }
 }
