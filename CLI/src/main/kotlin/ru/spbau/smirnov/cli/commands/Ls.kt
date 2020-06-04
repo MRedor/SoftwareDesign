bb52423fb598910e55636641a4d976e4ccc4c254
Maria Palchenkova <mashapalchenkova@gmail.com>
2020-06-04 12:36:17 +0300

    ls implemented

--- CLI/src/main/kotlin/ru/spbau/smirnov/cli/commands/Ls.kt
+++ CLI/src/main/kotlin/ru/spbau/smirnov/cli/commands/Ls.kt
@@ -2,10 +2,30 @@ package ru.spbau.smirnov.cli.commands
 
 import ru.spbau.smirnov.cli.Environment
 import ru.spbau.smirnov.cli.executor.Streams
+import java.io.DataOutputStream
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 
 class Ls(private val environment: Environment, arguments: List<String>) : Executable(arguments) {
     override fun execute(streams: Streams): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val output = DataOutputStream(streams.outputStream)
+        if (arguments.isEmpty()) {
+            print(Paths.get(environment.currentDirectory), streams)
+        } else {
+            for (arg in arguments) {
+                val path = Paths.get(environment.currentDirectory, arg)
+                if (!Files.isDirectory(path)) {
+                    streams.errorStream.println("Error in ls: argument is not a directory")
+                    return 1
+                }
+                output.writeBytes(arg + ": ${System.lineSeparator()}")
+                print(path, streams)
+                output.writeBytes("${System.lineSeparator()}")
+            }
+        }
+        return 0
     }
 
     override fun equals(other: Any?): Boolean {
@@ -18,5 +38,17 @@ class Ls(private val environment: Environment, arguments: List<String>) : Execut
         }
 
         return arguments == other.arguments
+    }
+
+    fun print(path: Path, streams: Streams) {
+        val output = DataOutputStream(streams.outputStream)
+
+        try {
+            path.toFile().list().forEach {
+                output.writeBytes(it.toString() + "${System.lineSeparator()}")
+            }
+        } catch (e: IOException) {
+            streams.errorStream.println("Error in ls.${System.lineSeparator()}${e.message}")
+        }
     }
 }
